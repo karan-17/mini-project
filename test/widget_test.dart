1d7f2393d2ffd06282746c01ad93a17dbbb2549b
Karan Vaswani <karanvaswani2002@gmail.com>
2022-01-14 18:48:45 +0530

    updated file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:miniproject/main.dart';
 
-import 'package:simlockapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
